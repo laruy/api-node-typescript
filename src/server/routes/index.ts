822136b3dcd1bd7405345f76ff3215489d8929fe
Lais Maruyama <laismaruyama@outlook.com>
2023-06-14 22:54:03 -0300

    feat: add folders structure

---
+++ src/server/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    return res.send('get');
+});
+
+router.post('/teste', (req, res) => {
+    console.log(req);
+    return res.status(StatusCodes.ACCEPTED).json(req.body);
+});
+
+export { router }
